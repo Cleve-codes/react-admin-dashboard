a67a34ae0b098f33a7170ec98ff6f3a29a21ddbc
Cleve <clevemomanyi@gmail.com>
2023-09-23 21:57:46 +0300

    init

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
-import { coloModeContex , useMode } from './theme';
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar';
 
 function App() {
-  const [theme, colorMode] = useMode()
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+        <CssBaseline />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/scenes/global/Topbar.jsx
+++ vite-project/src/scenes/global/Topbar.jsx
@@ -1,9 +1,24 @@
 // import React from 'react'
+import { Box, IconButton, useTheme } from '@mui/theme'
+import { useContext } from 'react'
+import { colorModeContext, tokens } from '../../theme'
+import InputBase from '@mui/material';
+import LightModeOutlinedIcon  from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlinedIcon";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlinedIcon";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlinedIcon";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlinedIcon";
+import searchIcon from "@mui/icons-material/search";
 
 function Topbar() {
+
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(colorModeContext)
+
   return (
-    <div>Topbar</div>
+    <Box>Topbar</Box>
   )
-}
+} 
 
 export default Topbar
